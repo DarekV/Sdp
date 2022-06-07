c246203ee7f2f6fe5df420edca2d3dcd10912dad
Darek Vassalli <darek.vassalli@studentfr.ch>
2022-06-07 11:05:28 +0200

    ajout image de transition finale

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var controller = new ScrollMagic.Controller();
 
-var transition = new TweenMax.to(".img_transition", 2, {
+var transition = new TweenMax.to(".transition_debut", 2, {
   y: -150,
 });
 
 var containerScene = new ScrollMagic.Scene({
   offset: 50,
   duration: 800,
+})
+  .setTween(transition)
+  .addIndicators()
+  .addTo(controller);
+
+
+  var controller = new ScrollMagic.Controller();
+
+var transition = new TweenMax.to(".transition_fin", 1, {
+  y: -150,
+});
+
+var containerScene = new ScrollMagic.Scene({
+  offset: 2500,
+  duration: 1500,
 })
   .setTween(transition)
   .addIndicators()
